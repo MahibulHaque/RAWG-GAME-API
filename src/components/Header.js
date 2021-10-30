21215ac20a4b02738c42f60155dab6fdd32ed39e
Mahibul Haque <mahibulhaque2000@gmail.com>
2021-10-30 23:04:08 +0600

    small tweaks in header.css

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaSearch, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+import Image from '../images/EA logo.svg';
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__icon"
+          src={Image}
+          alt=""
+        />
+      </Link>
+
+      <div className="header__center">
+        <FaSearch />
+        <input type="text" placeholder="Search"/>
+      </div>
+
+      <div className="header__right">
+        <FaUser />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
