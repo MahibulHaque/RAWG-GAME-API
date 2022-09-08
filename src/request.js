a792c0c45b989c3b890fe4e4f413ac8d10d71c56
Mahibul Haque <mahibulhaque2000@gmail.com>
2022-09-08 15:17:23 +0600

    Fix date format in request file

--- src/request.js
+++ src/request.js
@@ -1,31 +1,23 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-let today = new Date();
-let thisWeekDate = new Date(
-  today.getFullYear(),
-  today.getMonth(),
-  today.getDate()
-);
-let NextWeekDate = new Date(
-  today.getFullYear(),
-  today.getMonth(),
-  today.getDate() + 7
-);
-console.log(`${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`);
-console.log(
-  `${thisWeekDate.getFullYear()}-${thisWeekDate.getMonth()}-${thisWeekDate.getDate()}`
-);
-console.log(
-  `${NextWeekDate.getFullYear()}-${NextWeekDate.getMonth()}-${NextWeekDate.getDate()}`
-);
+const todayDate = new Date();
+
+const PreviousWeekDate = new Date();
+PreviousWeekDate.setDate(PreviousWeekDate.getDate()-7);
+
+const NextWeek = new Date();
+NextWeek.setDate(NextWeek.getDate()+7);
+
+const NextMonth = new Date();
+NextMonth.setMonth(NextMonth.getMonth()+1);
 
 const requests = {
   fetchTop10: `https://api.rawg.io/api/games?key=${API_KEY}&metacritic=70,100`,
   fetchActionGames: `https://api.rawg.io/api/games?key=${API_KEY}&genres=4`,
   fetchIndieGames: `https://api.rawg.io/api/games?key=${API_KEY}&genres=51&dates=2020-12-31,2021-9-31`,
-  fetchThisMonth: `https://api.rawg.io/api/games?key=${API_KEY}&dates=2021-09-06,${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`,
-  fetchThisWeek: `https://api.rawg.io/api/games?key=${API_KEY}&dates=2021-10-06,${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`,
-  fetchNextWeek: `https://api.rawg.io/api/games?key=${API_KEY}&dates=${today.getFullYear()}-${today.getMonth()}-${today.getDate()},${NextWeekDate.getFullYear()}-${NextWeekDate.getMonth()}-${NextWeekDate.getDate()}`,
+  fetchThisMonth: `https://api.rawg.io/api/games?key=${API_KEY}&dates=${todayDate.toISOString().split('T')[0]},${NextMonth.toISOString().split('T')[0]}`,
+  fetchThisWeek: `https://api.rawg.io/api/games?key=${API_KEY}&dates=${PreviousWeekDate.toISOString().split('T')[0]},${todayDate.toISOString().split('T')[0]}`,
+  fetchNextWeek: `https://api.rawg.io/api/games?key=${API_KEY}&dates=${NextWeek.toISOString().split('T')[0]},${NextWeek.toISOString().split('T')[0]}`,
   fetchRacingGames: `https://api.rawg.io/api/games?key=${API_KEY}&genres=1`,
   fetchAdventureGames: `https://api.rawg.io/api/games?key=${API_KEY}&genres=3`,
   fetchShooterGames: `https://api.rawg.io/api/games?key=${API_KEY}&genres=2&dates=2016-01-01,2021-09-21`,
